bug: stop normalizing mnist and simps; bug: make mnist have colour dimension 3 (by duplicating) as this changes the distances vs keeping it at 1; feat: add concat_patches as an option, where patch sizes are increase exponentially 6 times; refactor: make conv_abl not used by default, so pass --use_conv if want to use it

baselines.py
@@ -40,7 +40,6 @@ def compute_fractal_dimension(Z_, threshold=None):
     counts = []
     for size in sizes:
         counts.append(boxcount(Z, size))
-    print(counts)
 
     coeffs = np.polyfit(np.log(sizes), np.log(counts), 1)
     return -coeffs[0]
@@ -55,6 +54,8 @@ def jpg_compressed_size(im):
     x = im - im.min()
     x = im/im.max()
     x = (x*255).astype(np.uint8)
+    if x.shape[2] == 1:
+        x = np.squeeze(x,2)
     pim = Image.fromarray(x)
     compressed = BytesIO()
     pim.save(compressed,format='jpeg')

main.py
@@ -20,9 +20,9 @@ parser.add_argument('--display_cluster_imgs',action='store_true')
 parser.add_argument('--patch',action='store_true')
 parser.add_argument('--no_pretrained',action='store_true')
 parser.add_argument('--verbose','-v',action='store_true')
-#parser.add_argument('--display_images',action='store_true')
 parser.add_argument('--centroidify',action='store_true')
-parser.add_argument('--conv_abl',action='store_true')
+parser.add_argument('--use_conv',action='store_true')
+parser.add_argument('--concat_patches',action='store_true')
 parser.add_argument('--is_choose_model_per_dpoint',action='store_true')
 parser.add_argument('--dset',type=str,choices=['im','cifar','mnist','rand','dtd','stripes','halves'],default='stripes')
 parser.add_argument('--num_ims',type=int,default=1)
@@ -44,7 +44,6 @@ all_weighteds = []
 net = models.resnet18(pretrained=not ARGS.no_pretrained)
 comp_meas_kwargs = ARGS.__dict__
 comp_meas = ComplexityMeasurer(resnet=net,**comp_meas_kwargs)
-#comp_meas = ComplexityMeasurer(verbose=ARGS.verbose,ncs_to_check=ARGS.ncs_to_check,resnet=net,n_cluster_inits=ARGS.n_cluster_inits,display_cluster_imgs=ARGS.display_cluster_imgs)
 mean=[0.485, 0.456, 0.406]
 std=[0.229, 0.224, 0.225]
 weighted_by_class = {}
@@ -86,25 +85,18 @@ for i in range(ARGS.num_ims):
         elif ARGS.dset == 'mnist':
             im = numpyify(dset.data[i])
             im = np.array(Image.fromarray(im).resize((224,224)))
-            im = np.expand_dims(im,2)
+            im = np.tile(np.expand_dims(im,2),(1,1,3))
         label = int(dset.targets[i])
     if ARGS.display_cluster_imgs:
         plt.imshow(im);plt.show()
-    im_normed = (im-mean)/std
+    #im_normed = (im-mean)/std
+    im_normed = im
     greyscale_im = rgb2gray(im)
     fractal_dim = compute_fractal_dimension(greyscale_im)
-    print("Minkowski–Bouligand dimension (computed): ", fractal_dim)
     im_unint8 = (greyscale_im*255).astype(np.uint8)
     gclm_ent = glcm_entropy(im_unint8)
-    print("GLCM entropy: ", gclm_ent)
     ent = shannon_entropy(im_unint8)
-    print("entropy: ", ent)
-    #if ARGS.conv_abl:
-        #comp_meas.get_smallest_increment(im_normed)
-        #nc_in_image_itself,_,bayes_mdl = comp_meas.mdl_cluster(im_normed)
-        #all_assembly_idxs.append(nc_in_image_itself)
-    #else:
-    assembly_idx,level,bayes_mdl = comp_meas.interpret(im_normed,ARGS.conv_abl)
+    assembly_idx,level,bayes_mdl = comp_meas.interpret(im_normed)
     all_assembly_idxs.append(assembly_idx)
     all_levels.append(level)
     all_weighteds.append(bayes_mdl)

mdl_conv.py
@@ -15,13 +15,14 @@ from umap import UMAP
 PALETTE = list(BASE_COLORS.values()) + [(0,0.5,1),(1,0.5,0)]
 class ComplexityMeasurer():
     def __init__(self,verbose,ncs_to_check,resnet,n_cluster_inits,display_cluster_imgs,
-                 patch,conv_abl,is_choose_model_per_dpoint,nz,alg_nz,centroidify,**kwargs):
+                 patch,use_conv,is_choose_model_per_dpoint,nz,alg_nz,centroidify,concat_patches,**kwargs):
 
         self.verbose = verbose
         self.n_cluster_inits = n_cluster_inits
         self.display_cluster_imgs = display_cluster_imgs
         self.patch = patch
-        self.conv_abl = conv_abl
+        self.use_conv = use_conv
+        self.concat_patches = concat_patches
         self.is_choose_model_per_dpoint = is_choose_model_per_dpoint
         self.ncs_to_check = ncs_to_check
         self.nz = nz
@@ -41,7 +42,7 @@ class ComplexityMeasurer():
         self.dummy_initial_layer = nn.Sequential(make_dummy_layer(7,2,3),nn.MaxPool2d(3,2,1,dilation=1,ceil_mode=False))
         self.dummy_downsample_rlayer = make_dummy_layer(3,2,1)
 
-    def interpret(self,given_x,is_conv_abl):
+    def interpret(self,given_x):
         x = np.copy(given_x)
         total_num_clusters = 0
         total_weighted = 0
@@ -55,12 +56,18 @@ class ComplexityMeasurer():
             num_clusters_at_this_level, dl, weighted = self.mdl_cluster(x)
             total_num_clusters += num_clusters_at_this_level
             total_weighted += weighted
-            if is_conv_abl: break
             if self.centroidify:
                 full_im_size_means = [x[self.best_cluster_labels==c].mean(axis=0)
                                     for c in np.unique(self.best_cluster_labels)]
                 x = np.array(full_im_size_means)[self.best_cluster_labels]
-            x = self.apply_conv_layer(x,layer_being_processed)
+            if self.use_conv:
+                x = self.apply_conv_layer(x,layer_being_processed)
+            elif self.concat_patches:
+                patch_size = 2**layer_being_processed
+                x = patch_concats(x,patch_size)
+                print(patch_size,x.shape)
+            else:
+                break
         return total_num_clusters, layer_being_processed, total_weighted
 
     def get_smallest_increment(self,x):
@@ -80,14 +87,12 @@ class ComplexityMeasurer():
 
     def mdl_cluster(self,x_as_img):
         x = patch_averages(x_as_img) if self.patch else x_as_img
-        x = patch_concats(x)
         x = x.reshape(-1,x.shape[-1])
         assert x.ndim == 2
         N,nz = x.shape
         if nz > 50:
             x = PCA(50).fit_transform(x)
         if nz > 3:
-            dim_to_reduce_to = min(50,nz//2)
             if self.alg_nz == 'pca':
                 dim_reducer = PCA(self.nz)
             elif self.alg_nz == 'umap':
@@ -139,7 +144,7 @@ class ComplexityMeasurer():
             posterior_for_dset = softmax(log_likelihood_per_dpoint.sum(axis=0))
             weighted = np.dot(idxs_lens_array.sum(axis=0), posterior_for_dset)
         #print(f"votes for nc: {votes_for_nc}")
-        if not self.conv_abl:
+        if self.use_conv or self.concat_patches:
             print(f"weighted for layer {self.layer_being_processed}: {weighted:.2f}")
         return best_nc, best_dl, weighted
 
@@ -178,15 +183,10 @@ def make_dummy_layer(ks,stride,padding):
 def viz_proc_im(x):
     plt.imshow(x.sum(axis=2)); plt.show()
 
-def patch_concats(a):
-    try:
-        padded = np.pad(a,1)[:,:,1:-1]
-    except Exception as e:
-        print(e)
-        breakpoint()
-    different_shifts = [padded[:-1,:-1], padded[:-1,1:], padded[1:,:-1], padded[1:,1:]]
+def patch_concats(a,ps):
+    different_shifts = [a[:-ps,:-ps], a[:-ps,ps:], a[ps:,:-ps], a[ps:,ps:]]
     concatted = np.concatenate(different_shifts,axis=2)
-    return concatted[2:-2,2:-2]
+    return concatted
 
 def patch_averages(a):
     try: