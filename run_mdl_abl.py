0a912adbcdfc242f7fc167c8c1c5a79fd753e5bb
Lou1sM <oneillml@tcd.ie>
2022-05-09 15:00:30 +0100

    forgot to add the relevant file from the previous commit

--- run_mdl_abl.py
+++ run_mdl_abl.py
@@ -10,259 +10,254 @@ from os import listdir
 from get_dsets import load_fpath
 
 
-# im
-if sys.argv[1] == 'im':
-    with open(f'experiments/main_run_old/im_ims_used.txt') as f:
-        im_ims_used = [x.strip('\n') for x in f.readlines()]
-    with open(f'experiments/main_run_old/im_ARGS.txt') as f:
-        im_ARGS = f.readlines()
-        im_ARGS = dict([x.strip('\n').split(': ') for x in im_ARGS])
-        for k,v in im_ARGS.items():
-            if v == 'False':
-                im_ARGS[k] = False
-            elif v == 'True':
-                im_ARGS[k] = True
-            else:
-                try:
-                    im_ARGS[k] = int(v)
-                except ValueError:
-                    try:
-                        im_ARGS[k] = float(v)
-                    except ValueError:
-                        pass
-
-    comp_meas = ComplexityMeasurer(**im_ARGS)
-    comp_meas.is_mdl_abl = True
-    ims_list = []
-    for fname in im_ims_used:
-        print(fname)
-        class_dir = fname.split('_')[0]
-        if fname.startswith('ILSVRC'):
-            for class_dir in listdir('imagenette2/val'):
-                try:
-                    fpath = join('imagenette2/val',class_dir,fname)
-                    print('trying', fpath)
-                    im = load_fpath(fpath,is_resize=True)
-                    break
-                except: pass
-        else:
-            class_dir = fname.split('_')[0]
-            fpath = join('imagenette2/val',class_dir,fname)
-            im = load_fpath(fpath,is_resize=True)
-        if im.ndim == 2:
-            im = np.stack([im]*3,axis=2)
-        no_mdls, _, _ = comp_meas.interpret(im)
-        ims_list.append(no_mdls)
-
-    ims_arr = np.array(ims_list)
-    df = pd.DataFrame({'mean':ims_arr.mean(), 'var':ims_arr.var(), 'std':ims_arr.std(), 'raw':ims_list})
-    df.to_csv('experiments/main_run/im_no_mdl_abl.csv')
-
-elif sys.argv[1] == 'cifar':
-    # cifar
-    with open(f'experiments/main_run/cifar/cifar_ims_used.txt') as f:
-        cifar_ims_used = [x.strip('\n') for x in f.readlines()]
-    with open(f'experiments/main_run/cifar/cifar_ARGS.txt') as f:
-        cifar_ARGS = f.readlines()
-        cifar_ARGS = dict([x.strip('\n').split(': ') for x in cifar_ARGS])
-        for k,v in cifar_ARGS.items():
-            if v == 'False':
-                cifar_ARGS[k] = False
-            elif v == 'True':
-                cifar_ARGS[k] = True
-            else:
-                try:
-                    cifar_ARGS[k] = int(v)
-                except ValueError:
-                    try:
-                        cifar_ARGS[k] = float(v)
-                    except ValueError:
-                        pass
-
-    comp_meas = ComplexityMeasurer(**cifar_ARGS)
-    comp_meas.is_mdl_abl = True
-    results_list = []
-
-    torch_dset = torchvision.datasets.CIFAR10(root='~/datasets',download=True,train=True)
-    for i,im_used_idx in enumerate(cifar_ims_used[:2]):
-        print(i)
-        im = torch_dset.data[int(im_used_idx)]
-        im = np.array(Image.fromarray(im).resize((224,224)))/255
-        no_mdls, _, _ = comp_meas.interpret(im)
-        results_list.append(no_mdls)
-
+def save_from_results_list(results_list,dset):
     results_arr = np.array(results_list)
-    df = pd.DataFrame({'mean':results_arr.mean(), 'var':results_arr.var(), 'std':results_arr.std(), 'raw':results_list})
-    df.to_csv('experiments/main_run/cifar_no_mdl_abl.csv')
+    df = pd.DataFrame({'mean':results_arr.mean(), 'var':results_arr.var(), 'std':results_arr.std()},index=[dset])
+    df.to_csv(f'experiments/main_run/{dset}_no_mdl_abl.csv')
+    np.save(f'experiments/main_run/{dset}_no_mdl_abl_raw.npy',results_arr)
+    print(df)
 
-elif sys.argv[1] == 'dtd':
-    # dtd
-    with open(f'experiments/main_run/dtd_ARGS.txt') as f:
-        dtd_ARGS = f.readlines()
-        dtd_ARGS = dict([x.strip('\n').split(': ') for x in dtd_ARGS])
-        for k,v in cifar_ARGS.items():
-            if v == 'False':
-                dtd_ARGS[k] = False
-            elif v == 'True':
-                dtd_ARGS[k] = True
-            else:
-                try:
-                    dtd_ARGS[k] = int(v)
-                except ValueError:
+if __name__ == '__main__':
+    # im
+    if sys.argv[1] == 'im':
+        with open(f'experiments/main_run_old/im_ims_used.txt') as f:
+            im_ims_used = [x.strip('\n') for x in f.readlines()]
+        with open(f'experiments/main_run_old/im_ARGS.txt') as f:
+            im_ARGS = f.readlines()
+            im_ARGS = dict([x.strip('\n').split(': ') for x in im_ARGS])
+            for k,v in im_ARGS.items():
+                if v == 'False':
+                    im_ARGS[k] = False
+                elif v == 'True':
+                    im_ARGS[k] = True
+                else:
                     try:
-                        dtd_ARGS[k] = float(v)
+                        im_ARGS[k] = int(v)
                     except ValueError:
-                        pass
+                        try:
+                            im_ARGS[k] = float(v)
+                        except ValueError:
+                            pass
 
-    comp_meas = ComplexityMeasurer(**dtd_ARGS)
-    comp_meas.is_mdl_abl = True
-    ims_list = []
-    for fname in listdir('dtd/suitable'):
-        fpath = join('dtd/suitable',fname)
-        im = load_fpath(fpath,is_resize=True)
-        if im.ndim == 2:
-            im = np.stack([im]*3,axis=2)
-        no_mdls, _, _ = comp_meas.interpret(im)
-        ims_list.append(no_mdls)
-
-    ims_arr = np.array(ims_list)
-    df = pd.DataFrame({'mean':ims_arr.mean(), 'var':ims_arr.var(), 'std':ims_arr.std(), 'raw':ims_list})
-    df.to_csv('experiments/main_run/dtd_no_mdl_abl.csv')
-
-elif sys.argv[1] == 'mnist':
-    # mnist
-    with open(f'experiments/main_run_old/mnist/mnist_ims_used.txt') as f:
-        mnist_ims_used = [x.strip('\n') for x in f.readlines()]
-    with open(f'experiments/main_run_old/mnist_ARGS.txt') as f:
-        mnist_ARGS = f.readlines()
-        mnist_ARGS = dict([x.strip('\n').split(': ') for x in mnist_ARGS])
-        for k,v in mnist_ARGS.items():
-            if v == 'False':
-                mnist_ARGS[k] = False
-            elif v == 'True':
-                mnist_ARGS[k] = True
-            else:
-                try:
-                    mnist_ARGS[k] = int(v)
-                except ValueError:
+        comp_meas = ComplexityMeasurer(**im_ARGS)
+        comp_meas.is_mdl_abl = True
+        results_list = []
+        for fname in im_ims_used:
+            print(fname)
+            class_dir = fname.split('_')[0]
+            if fname.startswith('ILSVRC'):
+                for class_dir in listdir('imagenette2/val'):
                     try:
-                        mnist_ARGS[k] = float(v)
-                    except ValueError:
-                        pass
-
-    comp_meas = ComplexityMeasurer(**mnist_ARGS)
-    comp_meas.is_mdl_abl = True
-    ims_list = []
-    torch_dset = torchvision.datasets.CIFAR10(root='~/datasets',download=True,train=True)
-    for i,im_used_idx in enumerate(cifar_ims_used):
-        im = torch_dset.data[int(im_used_idx)]
-        im = np.array(Image.fromarray(im).resize((224,224)))/255
-        no_mdls, _, _ = comp_meas.interpret(im)
-        results_list.append(no_mdls)
-
-    ims_arr = np.array(ims_list)
-    df = pd.DataFrame({'mean':ims_arr.mean(), 'var':ims_arr.var(), 'std':ims_arr.std(), 'raw':ims_list})
-    df.to_csv('experiments/main_run/mnist_no_mdl_abl.csv')
-
-elif sys.argv[1] == 'stripes':
-    # stripes
-    with open(f'experiments/main_run/stripes/stripes_ARGS.txt') as f:
-        stripes_ARGS = f.readlines()
-        stripes_ARGS = dict([x.strip('\n').split(': ') for x in stripes_ARGS])
-        for k,v in stripes_ARGS.items():
-            if v == 'False':
-                stripes_ARGS[k] = False
-            elif v == 'True':
-                stripes_ARGS[k] = True
+                        fpath = join('imagenette2/val',class_dir,fname)
+                        print('trying', fpath)
+                        im = load_fpath(fpath,is_resize=True)
+                        break
+                    except: pass
             else:
-                try:
-                    stripes_ARGS[k] = int(v)
-                except ValueError:
+                class_dir = fname.split('_')[0]
+                fpath = join('imagenette2/val',class_dir,fname)
+                im = load_fpath(fpath,is_resize=True)
+            if im.ndim == 2:
+                im = np.stack([im]*3,axis=2)
+            no_mdls, _, _ = comp_meas.interpret(im)
+            results_list.append(sum(no_mdls))
+
+        save_from_results_list(results_list,'im')
+
+    # cifar
+    elif sys.argv[1] == 'cifar':
+        with open(f'experiments/main_run/cifar/cifar_ims_used.txt') as f:
+            cifar_ims_used = [x.strip('\n') for x in f.readlines()]
+        with open(f'experiments/main_run/cifar/cifar_ARGS.txt') as f:
+            cifar_ARGS = f.readlines()
+            cifar_ARGS = dict([x.strip('\n').split(': ') for x in cifar_ARGS])
+            for k,v in cifar_ARGS.items():
+                if v == 'False':
+                    cifar_ARGS[k] = False
+                elif v == 'True':
+                    cifar_ARGS[k] = True
+                else:
                     try:
-                        stripes_ARGS[k] = float(v)
+                        cifar_ARGS[k] = int(v)
                     except ValueError:
-                        pass
+                        try:
+                            cifar_ARGS[k] = float(v)
+                        except ValueError:
+                            pass
 
-    comp_meas = ComplexityMeasurer(**stripes_ARGS)
-    comp_meas.is_mdl_abl = True
-    ims_list = []
-    line_thicknesses = np.random.permutation(np.arange(3,10))
-    for i in range(500):
-        print(i)
-        slope = np.random.rand()+.5
-        line_thickness = line_thicknesses[i%len(line_thicknesses)]
-        im = create_simple_img('stripes',slope,line_thickness)
-        no_mdls, _, _ = comp_meas.interpret(im)
-        results_list.append(no_mdls)
+        comp_meas = ComplexityMeasurer(**cifar_ARGS)
+        comp_meas.is_mdl_abl = True
+        results_list = []
 
-    ims_arr = np.array(ims_list)
-    df = pd.DataFrame({'mean':ims_arr.mean(), 'var':ims_arr.var(), 'std':ims_arr.std(), 'raw':ims_list})
-    df.to_csv('experiments/main_run/stripes_no_mdl_abl.csv')
+        torch_dset = torchvision.datasets.CIFAR10(root='~/datasets',download=True,train=True)
+        for i,im_used_idx in enumerate(cifar_ims_used):
+            print(i)
+            im = torch_dset.data[int(im_used_idx)]
+            im = np.array(Image.fromarray(im).resize((224,224)))/255
+            no_mdls, _, _ = comp_meas.interpret(im)
+            results_list.append(sum(no_mdls))
 
-elif sys.argv[1] == 'halves':
-    # halves
-    with open(f'experiments/main_run_old/halves_ARGS.txt') as f:
-        halves_ARGS = f.readlines()
-        halves_ARGS = dict([x.strip('\n').split(': ') for x in halves_ARGS])
-        for k,v in halves_ARGS.items():
-            if v == 'False':
-                halves_ARGS[k] = False
-            elif v == 'True':
-                halves_ARGS[k] = True
-            else:
-                try:
-                    halves_ARGS[k] = int(v)
-                except ValueError:
+        save_from_results_list(results_list,'im')
+
+    elif sys.argv[1] == 'dtd':
+        # dtd
+        with open(f'experiments/main_run/dtd/dtd_ARGS.txt') as f:
+            dtd_ARGS = f.readlines()
+            dtd_ARGS = dict([x.strip('\n').split(': ') for x in dtd_ARGS])
+            for k,v in dtd_ARGS.items():
+                if v == 'False':
+                    dtd_ARGS[k] = False
+                elif v == 'True':
+                    dtd_ARGS[k] = True
+                else:
                     try:
-                        halves_ARGS[k] = float(v)
+                        dtd_ARGS[k] = int(v)
                     except ValueError:
-                        pass
+                        try:
+                            dtd_ARGS[k] = float(v)
+                        except ValueError:
+                            pass
 
-    comp_meas = ComplexityMeasurer(**halves_ARGS)
-    comp_meas.is_mdl_abl = True
-    ims_list = []
-    line_thicknesses = np.random.permutation(np.arange(3,10))
-    for i in range(500):
-        print(i)
-        slope = np.random.rand()+.5
-        im = create_simple_img('halves',slope,-1)
-        no_mdls, _, _ = comp_meas.interpret(im)
-        results_list.append(no_mdls)
+        comp_meas = ComplexityMeasurer(**dtd_ARGS)
+        comp_meas.is_mdl_abl = True
+        results_list = []
+        for i,fname in enumerate(listdir('dtd/suitable')):
+            print(i)
+            fpath = join('dtd/suitable',fname)
+            im = load_fpath(fpath,is_resize=True)
+            if im.ndim == 2:
+                im = np.stack([im]*3,axis=2)
+            no_mdls, _, _ = comp_meas.interpret(im)
+            results_list.append(sum(no_mdls))
 
-    ims_arr = np.array(ims_list)
-    df = pd.DataFrame({'mean':ims_arr.mean(), 'var':ims_arr.var(), 'std':ims_arr.std(), 'raw':ims_list})
-    df.to_csv('experiments/main_run/halves_no_mdl_abl.csv')
+        save_from_results_list(results_list,'dtd')
 
-elif sys.argv[1] == 'rand':
-    # rand
-    with open(f'experiments/main_run/rand/rand_ims_used.txt') as f:
-        rand_ims_used = [x.strip('\n') for x in f.readlines()]
-    with open(f'experiments/main_run/rand/rand_ARGS.txt') as f:
-        rand_ARGS = f.readlines()
-        rand_ARGS = dict([x.strip('\n').split(': ') for x in rand_ARGS])
-        for k,v in rand_ARGS.items():
-            if v == 'False':
-                rand_ARGS[k] = False
-            elif v == 'True':
-                rand_ARGS[k] = True
-            else:
-                try:
-                    rand_ARGS[k] = int(v)
-                except ValueError:
+    elif sys.argv[1] == 'mnist':
+        # mnist
+        with open(f'experiments/main_run_old/mnist/mnist_ims_used.txt') as f:
+            mnist_ims_used = [x.strip('\n') for x in f.readlines()]
+        with open(f'experiments/main_run_old/mnist_ARGS.txt') as f:
+            mnist_ARGS = f.readlines()
+            mnist_ARGS = dict([x.strip('\n').split(': ') for x in mnist_ARGS])
+            for k,v in mnist_ARGS.items():
+                if v == 'False':
+                    mnist_ARGS[k] = False
+                elif v == 'True':
+                    mnist_ARGS[k] = True
+                else:
                     try:
-                        rand_ARGS[k] = float(v)
+                        mnist_ARGS[k] = int(v)
                     except ValueError:
-                        pass
+                        try:
+                            mnist_ARGS[k] = float(v)
+                        except ValueError:
+                            pass
 
-    comp_meas = ComplexityMeasurer(**rand_ARGS)
-    comp_meas.is_mdl_abl = True
-    ims_list = []
-    for i in range(500):
-        print(i)
-        im = np.random.rand(224,224,3)
-        no_mdls, _, _ = comp_meas.interpret(im)
-        results_list.append(no_mdls)
+        comp_meas = ComplexityMeasurer(**mnist_ARGS)
+        comp_meas.is_mdl_abl = True
+        results_list = []
+        torch_dset = torchvision.datasets.CIFAR10(root='~/datasets',download=True,train=True)
+        for i,im_used_idx in enumerate(cifar_ims_used):
+            im = torch_dset.data[int(im_used_idx)]
+            im = np.array(Image.fromarray(im).resize((224,224)))/255
+            no_mdls, _, _ = comp_meas.interpret(im)
+            results_list.append(sum(no_mdls))
 
-    ims_arr = np.array(ims_list)
-    df = pd.DataFrame({'mean':ims_arr.mean(), 'var':ims_arr.var(), 'std':ims_arr.std(), 'raw':ims_list})
-    df.to_csv('experiments/main_run/rand_no_mdl_abl.csv')
+        save_from_results_list(results_list,'mnist')
+
+    elif sys.argv[1] == 'stripes':
+        # stripes
+        with open(f'experiments/main_run/stripes/stripes_ARGS.txt') as f:
+            stripes_ARGS = f.readlines()
+            stripes_ARGS = dict([x.strip('\n').split(': ') for x in stripes_ARGS])
+            for k,v in stripes_ARGS.items():
+                if v == 'False':
+                    stripes_ARGS[k] = False
+                elif v == 'True':
+                    stripes_ARGS[k] = True
+                else:
+                    try:
+                        stripes_ARGS[k] = int(v)
+                    except ValueError:
+                        try:
+                            stripes_ARGS[k] = float(v)
+                        except ValueError:
+                            pass
+
+        comp_meas = ComplexityMeasurer(**stripes_ARGS)
+        comp_meas.is_mdl_abl = True
+        results_list = []
+        line_thicknesses = np.random.permutation(np.arange(3,10))
+        for i in range(500):
+            print(i)
+            slope = np.random.rand()+.5
+            line_thickness = line_thicknesses[i%len(line_thicknesses)]
+            im = create_simple_img('stripes',slope,line_thickness)
+            no_mdls, _, _ = comp_meas.interpret(im)
+            results_list.append(sum(no_mdls))
+
+        save_from_results_list(results_list,'stripes')
+
+    elif sys.argv[1] == 'halves':
+        # halves
+        with open(f'experiments/main_run_old/halves_ARGS.txt') as f:
+            halves_ARGS = f.readlines()
+            halves_ARGS = dict([x.strip('\n').split(': ') for x in halves_ARGS])
+            for k,v in halves_ARGS.items():
+                if v == 'False':
+                    halves_ARGS[k] = False
+                elif v == 'True':
+                    halves_ARGS[k] = True
+                else:
+                    try:
+                        halves_ARGS[k] = int(v)
+                    except ValueError:
+                        try:
+                            halves_ARGS[k] = float(v)
+                        except ValueError:
+                            pass
+
+        comp_meas = ComplexityMeasurer(**halves_ARGS)
+        comp_meas.is_mdl_abl = True
+        results_list = []
+        line_thicknesses = np.random.permutation(np.arange(3,10))
+        for i in range(500):
+            print(i)
+            slope = np.random.rand()+.5
+            im = create_simple_img('halves',slope,-1)
+            no_mdls, _, _ = comp_meas.interpret(im)
+            results_list.append(sum(no_mdls))
+
+        save_from_results_list(results_list,'halves')
+
+    elif sys.argv[1] == 'rand':
+        # rand
+        with open(f'experiments/main_run/rand/rand_ims_used.txt') as f:
+            rand_ims_used = [x.strip('\n') for x in f.readlines()]
+        with open(f'experiments/main_run/rand/rand_ARGS.txt') as f:
+            rand_ARGS = f.readlines()
+            rand_ARGS = dict([x.strip('\n').split(': ') for x in rand_ARGS])
+            for k,v in rand_ARGS.items():
+                if v == 'False':
+                    rand_ARGS[k] = False
+                elif v == 'True':
+                    rand_ARGS[k] = True
+                else:
+                    try:
+                        rand_ARGS[k] = int(v)
+                    except ValueError:
+                        try:
+                            rand_ARGS[k] = float(v)
+                        except ValueError:
+                            pass
+
+        comp_meas = ComplexityMeasurer(**rand_ARGS)
+        comp_meas.is_mdl_abl = True
+        results_list = []
+        for i in range(500):
+            print(i)
+            im = np.random.rand(224,224,3)
+            no_mdls, _, _ = comp_meas.interpret(im)
+            results_list.append(sum(no_mdls))
+
+        save_from_results_list(results_list,'rand')
