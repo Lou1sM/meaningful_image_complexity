a7d2a7fbd6b23408bbe64dd92548cb983662249c
Lou1sM <oneillml@tcd.ie>
2022-05-10 13:14:41 +0100

    feat: split results csvs into main, ablations and scales

--- prepare_results.py
+++ prepare_results.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import math
 import sys
 from utils import filter_nans_from_df,make_alls_df
@@ -34,5 +35,28 @@ df = pd.concat(normed,axis=0,keys=df.index.levels[0])
 df = pd.concat([df.loc[k].T for k in df.index.levels[0]],keys=df.index.levels[0],axis=0)
 df = df.astype(float).round(4)
 print(df)
+df.to_csv(f'experiments/{exp_name}/all_results.csv')
 
-df.to_csv(f'experiments/{exp_name}/all_dsets.csv')
+main_methods = ['patch_ent','ent','fract','gclm','jpg','khan','mach','redies']
+abl_methods = ['patch_ent','no_mdl','ncs','mdl']
+scale_methods = [f'patch_ent{i}' for i in range(4)]
+
+def both_mean_std_as_array(df):
+    return np.array([f'{m} ({v})' for m,v in zip(df['mean'],df['std'])])
+
+df=df.drop(['var'],axis=1)
+df_main=df.loc[main_methods]
+both_as_arr = both_mean_std_as_array(df_main).reshape(len(main_methods),7)
+df_main=pd.DataFrame(both_as_arr,index=main_methods,columns=dsets)
+
+df_abls=df.loc[abl_methods]
+both_as_arr = both_mean_std_as_array(df_abls).reshape(len(abl_methods),7)
+df_abls=pd.DataFrame(both_as_arr,index=abl_methods,columns=dsets)
+
+df_scales=df.loc[scale_methods]
+both_as_arr = both_mean_std_as_array(df_scales).reshape(len(scale_methods),7)
+df_scales=pd.DataFrame(both_as_arr,index=scale_methods,columns=dsets)
+
+df_main.to_csv(f'experiments/{exp_name}/main_results.csv')
+df_abls.to_csv(f'experiments/{exp_name}/ablation_results.csv')
+df_scales.to_csv(f'experiments/{exp_name}/scales_results.csv')
