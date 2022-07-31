FEAT: add scales to glcm; BUG: fix grad computation for machado and redies

baselines.py
@@ -1,4 +1,5 @@
 from skimage.feature import graycomatrix
+import cv2
 import numpy as np
 from PIL import Image
 from io import BytesIO
@@ -6,6 +7,8 @@ from scipy import ndimage
 
 
 def rgb2gray(rgb):
+    if rgb.ndim == 2:
+        return rgb
     if rgb.shape[2] == 1:
         return rgb[:,:,0]
     r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
@@ -45,17 +48,24 @@ def compute_fractal_dimension(Z_, threshold=None):
     return -coeffs[0]
 
 def glcm_entropy(Z):
-    glcm = np.squeeze(graycomatrix(Z, distances=[1],
-                      angles=[0], symmetric=True,
-                      normed=True))
-    return -np.sum(glcm*np.log2(glcm + (glcm==0)))
+    glcm_at_scales = []
+    for dist in [1,4,8,16,32]:
+        glcm = np.squeeze(graycomatrix(Z, distances=[dist],
+                          angles=[0], symmetric=True,
+                          normed=True))
+        new_glcm_score = -np.sum(glcm*np.log2(glcm + (glcm==0)))
+        if new_glcm_score < 0:
+            breakpoint()
+        glcm_at_scales.append(new_glcm_score)
+    return sum(glcm_at_scales)
 
 def jpg_compressed_size(im):
     x = im - im.min()
     x = im/im.max()
     x = (x*255).astype(np.uint8)
-    if x.shape[2] == 1:
-        x = np.squeeze(x,2)
+    if x.ndim == 3:
+        if x.shape[2] == 1:
+            x = np.squeeze(x,2)
     pim = Image.fromarray(x)
     compressed = BytesIO()
     pim.save(compressed,format='jpeg')
@@ -67,16 +77,32 @@ def jpg_compression_ratio(im):
     return compressed_size/full_size
 
 def machado2015(im):
-    sob = ndimage.sobel(im)
+    sob = grad_mags(im)
     return jpg_compression_ratio(sob)
 
 def khan2021(im):
     return np.median(abs(np.fft.fft2((255*im).astype(np.uint8))))
 
 def redies2012(im):
-    sob = ndimage.sobel(im)
-    return jpg_compression_ratio(sob)
+    if im.ndim == 2:
+        max_mags = grad_mags(im)
+    else:
+        mag_by_channel = np.stack([grad_mags(im[:,:,i]) for i in range(im.shape[2])],axis=2)
+        max_mags = mag_by_channel.max(axis=2)
+    return max_mags.mean()
+
+def grad_mags(im):
+    #gx = cv2.Sobel(im, cv2.CV_32F, 1, 0, ksize=1)
+    #gy = cv2.Sobel(im, cv2.CV_32F, 0, 1, ksize=1)
+    #mag, angle = cv2.cartToPolar(gx, gy, angleInDegrees=True)
+    #return mag
+
+    sobx = ndimage.sobel(im,axis=1)
+    soby = ndimage.sobel(im,axis=0)
+    sob_mag = np.sqrt(np.square(sobx) + np.square(soby))
+    return sob_mag
+    #return jpg_compression_ratio(sob)
 
 if __name__ == "__main__":
     im = np.random.rand(224,224,3)
-    redies2012(im)
+    print(redies2012(im))